3c100d736b905950f213a3b5b8752c820f5198d5
lucky44x <nickbali06@gmail.com>
2025-09-22 17:14:56 +0200

    Started working on the combat integration of the AttackProfiles: - Wrote an entire selection function that selects the best AttackProfile based on chargetime - THROW THAT SHIT OUT, limit Player Attacks to one heavy and one light, to keep both on the same button (with charge time) - Implement a Combo System: either Procedural (Profiles define other Profiles that can follow it) - Explicit: Define ComboProfiles that dictate specific chains of inputs

--- scripts/entities/CombatController.gd
+++ scripts/entities/CombatController.gd
@@ -2,9 +2,18 @@
 extends Node
 class_name CombatController
 
+# Base State
+var interrupt_allowed: bool = true
 @export var hitscan_root: Node3D
 @export var parry_window: int
 @export var animator: AnimationPlayer
+
+# Attacking state
+var selected_profile: AttackProfile
+var current_profile_selection: Array[AttackProfile]
+var began_current_charge: int
+var current_charge_tier: int
+var current_charge_time: int
 
 # Blocking state
 var blocking_since: int = -1
@@ -14,7 +23,7 @@ var blocking_since: int = -1
 var hitscan_hits: Array[Node3D] = []
 
 @export_category("DEBUG")
-@export var profile: AttackProfile
+@export var debugProfile: AttackProfile
 @export_tool_button("Perform action", "Play") 
 var dbgButton = debug_perform_attack_profile
 
@@ -27,9 +36,6 @@ signal OnAttackFinished
 signal OnAnimationFinished
 
 # Attack related Signals
-## Gets called when an incoming attack has passed in status-effect changes
-## Sig: function(stat_type: [enum GameInfo.StatType], value: [float])
-signal OnStatChangeRecieved
 ## Gets called when an incoming attack was just parried
 signal OnIncomingAttackParried
 ## Gets called when an incoming attack was just blocked
@@ -42,21 +48,29 @@ signal OnOutgoingAttackBlocked
 func _ready():
 	pass
 
-## TODO: REMOVE -- THIS IS ENTIRELY DEBUG SPECIFIC
+## TODO: Stop debug code from running in actual build
 func _process(_delta: float):
-	if profile != null && len(active_scan_zones) != len(profile.colliders): internal_set_profile(profile)
+	if not Engine.is_editor_hint():
+		internal_charge_tick()
+		return
 	
-	if profile != null:
-		for coll in profile.colliders:
+	if debugProfile != null && len(active_scan_zones) != len(debugProfile.colliders): internal_set_profile(debugProfile)
+	
+	if debugProfile != null:
+		for coll in debugProfile.colliders:
 			if coll == null:
-				DebugDraw3D.draw_text(hitscan_root.global_position, "COLLIDER NULL " + str(profile.colliders.find(coll)), 128)
+				DebugDraw3D.draw_text(hitscan_root.global_position, "COLLIDER NULL " + str(debugProfile.colliders.find(coll)), 128)
 				continue
 			if coll.Shape == null:
-				DebugDraw3D.draw_text(hitscan_root.global_position, "SHAPE NULL " + str(profile.colliders.find(coll)), 128)
+				DebugDraw3D.draw_text(hitscan_root.global_position, "SHAPE NULL " + str(debugProfile.colliders.find(coll)), 128)
 				continue
 			
 			var color = Color.RED
-			if active_scan_zones[profile.colliders.find(coll)]: color = Color.GREEN
+			if active_scan_zones[debugProfile.colliders.find(coll)]:
+				if interrupt_allowed: color = Color.GREEN
+				else: color = Color.SLATE_GRAY
+			elif not interrupt_allowed: color = Color.SALMON
+			
 			var pos = hitscan_root.global_position + (hitscan_root.global_basis.x.normalized() * coll.Position.x) + (hitscan_root.global_basis.y.normalized() * coll.Position.y) + (hitscan_root.global_basis.z.normalized() * coll.Position.z)
 			var rot = Quaternion.from_euler(hitscan_root.global_rotation + Vector3(deg_to_rad(coll.Rotation.x), deg_to_rad(coll.Rotation.y), deg_to_rad(coll.Rotation.z)))
 			var up_vec = Basis.from_euler(rot.get_euler()).y
@@ -75,28 +89,76 @@ func _process(_delta: float):
 			elif coll.Shape is CylinderShape3D:
 				DebugDraw3D.draw_cylinder_ab(pos - up_vec * (coll.Shape.height/2), pos + up_vec * (coll.Shape.height/2), coll.Shape.radius, color)
 
-func debug_perform_attack_profile():
-	if not Engine.is_editor_hint(): return
-	if profile == null:
-		DebugDraw3D.draw_text(hitscan_root.global_position + Vector3.UP * 2, "No profile set", 64, Color.RED, 5)
-		return
-	if not animator.has_animation(profile.animation):
-		DebugDraw3D.draw_text(hitscan_root.global_position + Vector3.UP * 2, "Animation " + profile.animation + " not found", 64, Color.RED, 5)
-		return
-	
-	if profile.attack_type == GameInfo.AttackType.Light:
-		animator.play(profile.animation)
-	else: debug_perform_heavy_attack(profile)
-
-func debug_perform_heavy_attack(attackProfile: AttackProfile):
-	if attackProfile.attack_type == GameInfo.AttackType.Light: return
-	animator.play(profile.charging_entry_animation)
-	animator.queue(profile.animation)
-
 func internal_set_profile(prof: AttackProfile):
 	active_scan_zones.clear()
 	active_scan_zones.resize(len(prof.colliders))
 	active_scan_zones.fill(false)
+
+#region Debug Editor Functionallity
+func debug_perform_attack_profile():
+	if not Engine.is_editor_hint(): return
+	if debugProfile == null:
+		DebugDraw3D.draw_text(hitscan_root.global_position + Vector3.UP * 2, "No debugProfile set", 64, Color.RED, 5)
+		return
+	if not animator.has_animation(debugProfile.animation):
+		DebugDraw3D.draw_text(hitscan_root.global_position + Vector3.UP * 2, "Animation " + debugProfile.animation + " not found", 64, Color.RED, 5)
+		return
+	
+	if debugProfile.attack_type == GameInfo.AttackType.Light:
+		animator.play(debugProfile.animation)
+	else: debug_perform_heavy_attack(debugProfile)
+
+func debug_perform_heavy_attack(attackProfile: AttackProfile):
+	if attackProfile.attack_type == GameInfo.AttackType.Light: return
+	animator.play(debugProfile.charging_entry_animation)
+	animator.queue(debugProfile.animation)
+#endregion
+
+#region Attacking
+
+func BeginCharge(profile_selection: Array[AttackProfile]):
+	internal_set_profile(debugProfile)
+	current_profile_selection = profile_selection
+	began_current_charge = Time.get_ticks_msec()
+
+func internal_charge_tick():
+	if current_profile_selection == null: return
+	var elapsed: int = Time.get_ticks_msec() - began_current_charge
+	
+	var foundProfile: AttackProfile = null
+	for profile in current_profile_selection:
+		# Filter out light attacks, as they are not charged at all
+		# If no other profile selected, selected light attack as default selection
+		if profile.attack_type == GameInfo.AttackType.Light:
+			if foundProfile == null: foundProfile = profile
+			continue
+		
+		# Loop over charge tiers to determine highest charge_tier
+		for tier in profile.charging_tiers:
+			if tier.Required_Time_ms > elapsed:
+				# Any checks at tiers beyond here would be waste resources
+				break
+			var tmp_tier: int = profile.charging_tiers.find(tier)
+			if tier.Required_Time_ms > current_charge_time: # When charge tier found that meets time requirements and takes more "effort" than current tier, select it
+				foundProfile = profile
+				current_charge_tier = tmp_tier
+				current_charge_time = tier.Required_Time_ms
+	
+	selected_profile = foundProfile		# Set selected profile to found profile
+
+func CommenceAttack():
+	BeginAttack(selected_profile, current_charge_tier)
+
+func BeginAttack(profile: AttackProfile, charge_tier: int = 0):
+	pass
+
+func AbortAttack() -> bool:
+	if interrupt_allowed:
+		pass
+	
+	return interrupt_allowed
+
+#endregion
 
 # Exposed and used by attacking combat-controller
 func IsBlocked() -> bool:
@@ -111,10 +173,6 @@ func StartBlocking():
 
 func EndBlocking():
 	pass
-
-## Applies the given value to a given stat
-func ApplyStatChange(type: GameInfo.StatType, value: float):
-	OnStatChangeRecieved.emit(type, value)
 
 ## Applies a given time to a given effect
 func ApplyEffect():
@@ -134,5 +192,13 @@ func internal_begin_hitscan(zone: int):
 ## Internal and used by animator for hitscanning
 func internal_end_hitscan(zone: int):
 	active_scan_zones[zone] = false
+
+# Locks the interrupt, committing the player to the animation and not allowing breaking out of it
+func internal_lock_interrupt():
+	interrupt_allowed = false
+
+# Unlocks the interrupt, allowing the player to cancel out of the attack animation
+func internal_unlock_interrupt():
+	interrupt_allowed = true
 
 # DEBUG
--- scripts/resources/WeaponData.gd
+++ scripts/resources/WeaponData.gd
@@ -2,3 +2,5 @@ extends ItemData
 class_name WeaponData
 
 @export var hit_modifiers: Array[StatModifier]
+@export var basic_light_attack: AttackProfile
+@export var basic_heavy_attack: AttackProfile
